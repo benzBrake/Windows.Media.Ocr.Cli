5da99bf9ce89f7ac41708aa12cfc73971ce168f4
Ryan Lieu <github-benzBrake@woai.ru>
2026-06-04 22:02:19 +0800

    Fix local build against installed Windows SDK

--- Windows.Media.Ocr.Cli/Program.cs
+++ Windows.Media.Ocr.Cli/Program.cs
@@ -14,6 +14,7 @@ using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Graphics.Imaging;
+using Windows.Media.Ocr;
 using Windows.Storage;
 using Windows.Storage.Streams;
 
